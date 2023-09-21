71bedf72545afb42dc2f5c0139555e468d73efda
12Harsha2000 <harshamariajose.20@gmail.com>
2023-09-21 11:58:25 +0530

    initiated

---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import ContactlistComponent from "./components/ContactlistComponents";
+import ConversationComponent from "./components/ConversationComponents";
+
+const Container = styled.div`
+display : flex;
+flex-direction: row;
+height: 100vh;
+width: 100%;
+background: #f8f9fb;
+`
+
+function App() {
+  return <Container>
+    <ContactlistComponent/>
+    <ConversationComponent/>
+  </Container> 
+}
+
+export default App;
---
+++ whatsapp-clone/src/components/ContactlistComponents.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display:flex;
+flex-direction: coloumn;
+height:100%
+flex: 1.6;
+`
+const ContactlistComponent = () => {
+    return <Container>"ContactlistComponent"</Container>
+}
+
+export default ContactlistComponent;
