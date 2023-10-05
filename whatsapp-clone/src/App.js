2839d9eb91a4947855a73fab285f053022691f1a
12Harsha2000 <harshamariajose.20@gmail.com>
2023-10-05 16:27:19 +0530

    Initiated

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,22 +1,25 @@
-import styled from "styled-components";
-import ContactlistComponent from "./components/ContactlistComponents";
-import ConversationComponent from "./components/ConversationComponents";
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import ContactListComponent from './components/ContactlistComponents';
+import ConversationComponent from './components/ConversationComponents';
+import { contactList, messagesList } from './Data';
 
 const Container = styled.div`
-display : flex;
-flex-direction: row;
-height: 100vh;
-width: 100%;
-background: #f8f9fb;
-`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+  width: 100%;
+  background: #f8f9fb;
+`;
 
 function App() {
+  const [selectedContact, setSelectedContact] = useState(null);
+
   return (
-  <Container>
-    <ContactlistComponent/>
-    <ConversationComponent/>
-  </Container> 
+    <Container>
+      <ContactListComponent onSelectContact={(contact) => setSelectedContact(contact)} />
+      {selectedContact && <ConversationComponent selectedContact={selectedContact} messages={messagesList} />}
+    </Container>
   );
 }
 
